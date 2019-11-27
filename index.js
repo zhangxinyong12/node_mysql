6252d048e2fc1a933b499f1ad9267c26594e2db9
张新永 <xinyong.zhang1@baozun.com>
2019-11-27 15:26:49 +0800

    增 删 改 查

--- index.js
+++ index.js
@@ -8,10 +8,33 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
-const addSql = ``;
-const sql = `SELECT * FROM websites`;
+// 新增
+const addSql = `INSERT INTO websites (Id, name, url, alexa, country) VALUES (0,?,?,?,?)`;
+const addSqlParams = ['菜鸟工具', 'https://c.runoob.com', '23453', 'CN'];
+connection.query(addSql, addSqlParams, (error, result) => {
+    if (error) throw error;
+    console.log('add', result);
+});
 
+// 修改
+const modSql = `UPDATE websites SET name = ?, url = ? WHERE Id = ?`;
+const modSqlParams = ['菜鸟移动站', 'https://m.runoob.com', 6];
+// connection.query(modSql, modSqlParams, (err, result) => {
+//     console.log('update', result.affectedRows);
+// });
+
+// 删除
+const delSql = `DELETE FROM websites where id = 7`;
+// connection.query(delSql, (err, result) => {
+//     console.log('delete', result);
+// });
+
+// 查询
+const sql = `SELECT * FROM websites`;
 connection.query(sql, (error, results, fields) => {
     if (error) throw error;
     console.log(results);
 });
+
+// 关闭
+connection.end();
